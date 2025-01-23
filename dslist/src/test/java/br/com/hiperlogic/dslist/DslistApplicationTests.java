fade8eedfe217c936d85689ede99bfb85b6fa40b
Rodrigo Domingues <rodrigo@gamegesis.com>
2025-01-23 16:57:17 -0300

    Iniciando Repo

---
+++ dslist/src/test/java/br/com/hiperlogic/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hiperlogic.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
